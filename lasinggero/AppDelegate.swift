46aa709b3602a59bbf02dc724759609c557587e6
Marvin Allan Lu <nattefrost@Marvins-MacBook-Air.local>
2015-10-24 04:09:40 +0800

    Updates UI

--- lasinggero/AppDelegate.swift
+++ lasinggero/AppDelegate.swift
@@ -21,6 +21,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
         
+        UINavigationBar.appearance().barTintColor = UIColor(red: 52.0/255.0, green: 52.0/255.0, blue: 59.0/255.0, alpha: 1.0)
+        UINavigationBar.appearance().tintColor = UIColor.whiteColor()
+        UINavigationBar.appearance().titleTextAttributes = [NSForegroundColorAttributeName : UIColor.whiteColor()]
         
         UILabel.appearance().substituteFontName = "Avenir"
         let userDefault = NSUserDefaults.standardUserDefaults()
